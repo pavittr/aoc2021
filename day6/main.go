d880a39ffadda70fda611c0ef13a5ab090af9d13
Ross Pavitt <ross.pavitt@gmail.com>
2021-12-06 08:52:07 +0000

    Day6

--- day6/main.go
+++ day6/main.go
@@ -12,130 +12,45 @@ import (
 	"github.com/pavittr/aoc2021/utils"
 )
 
+type Fish int64
+
+func (f Fish) proceed() []Fish {
+	if f == 0 {
+		return []Fish{6,8}
+	}
+	return []Fish{f -1}
+}
 func main() {
 	if err := utils.RunDay(solution1, solution2); err != nil {
 		log.Fatalf("Failed to run day: %+v\n", err)
 	}
 }
 
-type Coord struct {
-	X int
-	Y int
-}
-
-type Line []Coord
-
-func coordFrom(coordStr string) (Coord, error) {
-	xy := strings.Split(coordStr, ",")
-	x, xErr := strconv.Atoi(xy[0])
-	if xErr != nil {
-		return Coord{}, fmt.Errorf("failed to parse x from %+v of line %+v: %+v\n", coordStr, xy, xErr)
-	}
-	y, yErr := strconv.Atoi(xy[1])
-	if yErr != nil {
-		return Coord{}, fmt.Errorf("failed to parse y from %+v of line %+v: %+v\n", coordStr, xy, yErr)
-	}
-	return Coord{x, y}, nil
-}
-func fromLine(line string,  includeDiagonals bool) (Line, error) {
-	coords := strings.Split(line, " -> ")
-	src, srcErr := coordFrom(coords[0])
-	if srcErr != nil {
-		return Line{}, fmt.Errorf("faile to parse src from line %s: %+v", line, srcErr)
-	}
-	dst, dstErr := coordFrom(coords[1])
-	if dstErr != nil {
-		return Line{}, fmt.Errorf("faile to parse dst from line %s: %+v", line, dstErr)
-	}
-
-	retLine := make(Line, 0)
-	if src.X == dst.X {
-		if src.Y > dst.Y {
-			tmp := dst
-			dst = src
-			src = tmp
-		}
-		for y := src.Y; y <= dst.Y; y++ {
-			retLine = append(retLine, Coord{src.X, y})
-		}
-	} else if src.Y == dst.Y {
-		if src.X > dst.X {
-			tmp := dst
-			dst = src
-			src = tmp
-		}
-
-		for x := src.X; x <= dst.X; x++ {
-			retLine = append(retLine, Coord{x, src.Y})
-		}
-	} else if includeDiagonals {
-		if src.X < dst.X && src.Y < dst.Y {
-			for x, y := src.X, src.Y; x <= dst.X; x++ {
-				retLine = append(retLine, Coord{x, y})
-				y++
-			}
-		} else if src.X > dst.X && src.Y < dst.Y {
-			for x, y := src.X, src.Y; x >= dst.X; x-- {
-				retLine = append(retLine, Coord{x, y})
-				y++
-			}
-	} else if src.X < dst.X && src.Y > dst.Y {
-		for x, y := src.X, src.Y; x <= dst.X; x++ {
-				retLine = append(retLine, Coord{x, y})
-				y--
-			}
-	} else if src.X > dst.X && src.Y > dst.Y {
-		for x, y := src.X, src.Y; x >= dst.X; x-- {
-				retLine = append(retLine, Coord{x, y})
-				y--
-			}
-		}
-
-	}
-
-	return retLine, nil
-
-}
 func solution1(puzzleInput string) (string, error) {
-	txtLines := strings.Split(strings.TrimSpace(puzzleInput), "\n")
-	overlap := map[Coord]int{}
-	for _, txtLine := range txtLines {
-		line, lineErr := fromLine(txtLine, false)
-		if lineErr != nil {
-			return "", fmt.Errorf("failed to pro ess line %s: %+v", txtLine, lineErr)
+	txtLines := strings.Split(strings.TrimSpace(puzzleInput), ",")
+	shoal := make([]Fish, 0)
+	for _, element := range txtLines {
+		r, fErr := strconv.Atoi(element)
+		if fErr != nil {
+			return "", fmt.Errorf("failed to turn %s into an int: %+v", element, fErr)
 		}
-		for _, coord := range line {
-			overlap[coord]++
-		}
+		
+		shoal = append(shoal, Fish(r))
 	}
-	counter := 0
-	for _, count := range overlap {
-		if count > 1 {
-			counter++
+	nextShoal := make([]Fish,0)
+	for i:= 0 ;i< 256 ; i++ {
+		for _, fish := range shoal {
+			nextShoal = append(nextShoal, fish.proceed()...)
 		}
+		shoal = nextShoal
+		nextShoal = make([]Fish, 0)
 	}
+	log.Printf("shial %d\n", len(shoal))
 
-	return fmt.Sprintf("%d", counter), nil
+	return fmt.Sprintf(""), nil
 }
 
 func solution2(puzzleInput string) (string, error) {
-	txtLines := strings.Split(strings.TrimSpace(puzzleInput), "\n")
-	overlap := map[Coord]int{}
-	for _, txtLine := range txtLines {
-		line, lineErr := fromLine(txtLine, true)
-		if lineErr != nil {
-			return "", fmt.Errorf("failed to pro ess line %s: %+v", txtLine, lineErr)
-		}
-		for _, coord := range line {
-			overlap[coord]++
-		}
-	}
-	counter := 0
-	for _, count := range overlap {
-		if count > 1 {
-			counter++
-		}
-	}
 
-	return fmt.Sprintf("%d", counter), nil
+	return fmt.Sprintf(""), nil
 }
